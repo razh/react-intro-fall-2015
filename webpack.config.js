aa16088558a23abe2b4530b2ea25422d9f839cd4
Raymond Zhou <somecrazyidiot@gmail.com>
2015-09-26 12:57:52 -0400

    Update dependencies
    - Update react-transform configuration format.
    - Include CSS modules.
    - Add basic CDN stylesheets.

--- webpack.config.js
+++ webpack.config.js
@@ -8,25 +8,42 @@ module.exports = {
 
   entry: [
     'webpack-hot-middleware/client',
-    './' + DIR + '/index'
+    './' + DIR + '/index',
   ],
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
 
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, DIR)
-    }]
-  }
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, DIR),
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss',
+        ],
+        include: path.join(__dirname, DIR),
+      },
+    ],
+  },
+
+  postcss() {
+    return [
+      require('autoprefixer'),
+    ];
+  },
 };
